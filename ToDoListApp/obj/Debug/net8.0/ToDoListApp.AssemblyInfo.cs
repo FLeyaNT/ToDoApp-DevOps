eb45701b640ecdb28ae4fb71484f93ed4fff82f7
FLeyaNT <ilitavor@mail.ru>
2024-11-17 12:53:57 +0300

    V2

--- ToDoListApp/obj/Debug/net8.0/ToDoListApp.AssemblyInfo.cs
+++ ToDoListApp/obj/Debug/net8.0/ToDoListApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd95018c161326d6cee72b84e05355ff7f52705")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ToDoListApp/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿let items = document.querySelectorAll('.form-container');
+
+for (let i = 0; i < items.length; i++) {
+    const element = items[i];
+    element.style.transitionDuration = `${3 + i}s`;
+}
+
+document.addEventListener('onbeforeunload', function () {
+    setTimeout(() => {
+        console.log("Загрузка");
+    }, 1000);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    items.forEach(item => {
+        item.classList.add('appeared');
+    })
+});
